1694dc78bf55c60194ea10694a21bbccad9339b0
Jacques Domenie <domenie@outlook.com>
2026-06-01 22:20:10 +0200

    language and google

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-
 import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.mkbtechgids.nl',
   output: 'server',
-
   security: {
     checkOrigin: false
   },
-
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
   },
-
   adapter: vercel()
 });
