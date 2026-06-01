f97b15805eb5ed4a2c21290e9e1f0aafa3a12130
Jacques Domenie <domenie@outlook.com>
2026-06-01 21:16:54 +0200

    brevo werken

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+
+  security: {
+    checkOrigin: false
+  },
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: vercel()
 });
